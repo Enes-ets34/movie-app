a3859faa6d3630f4446f7f5095dbff06ff9ea11b
Enes Taha Sarı <ets.34.es@gmail.com>
2025-01-11 18:03:30 +0300

    feat: add form validation

--- src/screens/login/login.types.ts
+++ src/screens/login/login.types.ts
@@ -1,3 +1,13 @@
+import { Control, FieldErrors, UseFormTrigger } from 'react-hook-form';
+
+export interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
 export interface LoginProps {
-  value?: string;
+  control: Control<LoginFormValues>; 
+  errors: FieldErrors<LoginFormValues>;
+  handleSubmit: () => void;
+  trigger: UseFormTrigger<LoginFormValues>; // trigger fonksiyonu da ekleniyor
 }
